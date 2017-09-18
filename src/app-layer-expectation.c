9c5d93c2bf840668f5e465c3e886721aef44a5e2
Eric Leblond <eric@regit.org>
2017-09-19 00:27:31 +0200

    app-layer-expectation: fix code

--- src/app-layer-expectation.c
+++ src/app-layer-expectation.c
@@ -41,11 +41,12 @@ typedef struct _Expectation {
     Port sp;
     Port dp;
     AppProto alproto;
+    int direction;
     void *data;
     struct _Expectation *next;
 } Expectation;
 
-static void ExpectationFree(void *e)
+static void ExpectationListFree(void *e)
 {
     Expectation *exp = (Expectation *)e;
     Expectation *lexp;
@@ -66,7 +67,7 @@ static void ExpectationDataFree(void *e)
 
 void AppLayerExpectationSetup(void)
 {
-    expectation_id = IPPairStorageRegister("expectation", sizeof(void *), NULL, ExpectationFree);
+    expectation_id = IPPairStorageRegister("expectation", sizeof(void *), NULL, ExpectationListFree);
     expectation_data_id = FlowStorageRegister("expectation", sizeof(void *), NULL, ExpectationDataFree);
 }
 
@@ -101,16 +102,17 @@ int AppLayerExpectationCreate(Flow *f, int direction, Port src, Port dst, AppPro
     exp->alproto = alproto;
     exp->ts = f->lastts;
     exp->data = data;
+    exp->direction = direction;
 
     if (GetFlowAddresses(f, &ip_src, &ip_dst) == -1)
-        return -1;
+        goto error;
     if (direction & STREAM_TOSERVER) {
         ipp = IPPairGetIPPairFromHash(&ip_src, &ip_dst);
     } else {
         ipp = IPPairGetIPPairFromHash(&ip_dst, &ip_src);
     }
     if (ipp == NULL)
-        return -1;
+        goto error;
 
     iexp = IPPairGetStorageById(ipp, expectation_id);
     exp->next = iexp;
@@ -118,6 +120,11 @@ int AppLayerExpectationCreate(Flow *f, int direction, Port src, Port dst, AppPro
 
     IPPairUnlock(ipp);
     return 0;
+
+error:
+    if (exp != NULL)
+        SCFree(exp);
+    return -1;
 }
 
 static Expectation *AppLayerExpectationGet(Flow *f, int direction, IPPair **ipp)
@@ -142,18 +149,19 @@ int AppLayerExpectationGetDataId(void)
     return expectation_data_id;
 }
 
-static void RemoveExpectation(IPPair *ipp, Expectation *pexp, Expectation *exp,
-                              Expectation *lexp)
+static Expectation * RemoveExpectationAndGetNext(IPPair *ipp,
+                                Expectation *pexp, Expectation *exp,
+                                Expectation *lexp)
 {
     if (pexp == NULL) {
-        if (lexp != NULL) {
-            IPPairSetStorageById(ipp, expectation_id, lexp);
-        }
+        IPPairSetStorageById(ipp, expectation_id, lexp);
     } else {
         pexp->next = lexp;
     }
+    if (exp->data)
+        SCFree(exp->data);
     SCFree(exp);
-    exp = lexp;
+    return lexp;
 }
 
 AppProto AppLayerExpectationLookup(Flow *f, int direction)
@@ -171,24 +179,24 @@ AppProto AppLayerExpectationLookup(Flow *f, int direction)
     pexp = NULL;
     while (exp) {
         lexp = exp->next;
-        if ((exp->sp == 0) || (exp->sp == f->sp)) {
-            if ((exp->dp == 0) || (exp->dp == f->dp)) {
-                alproto = exp->alproto;
-                if (FlowSetStorageById(f, expectation_data_id, exp->data) != 0) {
-                    SCLogError(SC_ERR_UNKNOWN_VALUE, "Unable to set flow storage");
-                }
-                exp->data = NULL;
-                (void) IPPairDecrUsecnt(ipp);
-                /* remove the expectation */
-                RemoveExpectation(ipp, pexp, exp, lexp);
-                continue;
+        if ( (exp->direction & direction) &&
+             ((exp->sp == 0) || (exp->sp == f->sp)) &&
+             ((exp->dp == 0) || (exp->dp == f->dp))) {
+            alproto = exp->alproto;
+            if (FlowSetStorageById(f, expectation_data_id, exp->data) != 0) {
+                SCLogError(SC_ERR_UNKNOWN_VALUE, "Unable to set flow storage");
             }
+            exp->data = NULL;
+            (void) IPPairDecrUsecnt(ipp);
+            /* remove the expectation */
+            exp = RemoveExpectationAndGetNext(ipp, pexp, exp, lexp);
+            continue;
         }
         /* Cleaning remove old entries */
         if (exp && (ctime > exp->ts.tv_sec + EXPECTATION_TIMEOUT)) {
             (void) IPPairDecrUsecnt(ipp);
             /* remove the expectation */
-            RemoveExpectation(ipp, pexp, exp, lexp);
+            exp = RemoveExpectationAndGetNext(ipp, pexp, exp, lexp);
             continue;
         }
         pexp = exp;
