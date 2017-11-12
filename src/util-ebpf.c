4a8d8c06cc5bab7bb3f1a50e76a65c5fc18a6ceb
Eric Leblond <eric@regit.org>
2017-11-12 15:53:39 +0100

    util-ebpf: suppress call on loop init

--- src/util-ebpf.c
+++ src/util-ebpf.c
@@ -199,9 +199,6 @@ int EBPFForEachFlowV4Table(const char *name,
     unsigned int nr_cpus = UtilCpuGetNumProcessorsConfigured();
     struct pair values_array[nr_cpus];
 
-    if (bpf_map_get_next_key(mapfd, &key, &next_key) != 0) {
-        return found;
-    }
     while (bpf_map_get_next_key(mapfd, &key, &next_key) == 0) {
         int iret = 1;
         int pkts_cnt = 0;
@@ -243,9 +240,6 @@ int EBPFForEachFlowV6Table(const char *name,
     unsigned int nr_cpus = UtilCpuGetNumProcessorsConfigured();
     struct pair values_array[nr_cpus];
 
-    if (bpf_map_get_next_key(mapfd, &key, &next_key) != 0) {
-        return found;
-    }
     while (bpf_map_get_next_key(mapfd, &key, &next_key) == 0) {
         int iret = 1;
         int pkts_cnt = 0;
