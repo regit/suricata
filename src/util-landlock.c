4845b65bf2c3c1853784222a84127c48ddc160b4
Eric Leblond <el@stamus-networks.com>
2026-07-29 12:04:56 +0200

    landlock: support older kernel headers
    struct landlock_ruleset_attr gained the scoped field in Linux 6.12
    (Landlock ABI 6) and handled_access_net in Linux 6.7 (ABI 4). Missing
    constants can be defined by hand, a struct member cannot, so both need
    their own configure check and the code using them has to be guarded.
    
    Ticket #8606

--- src/util-landlock.c
+++ src/util-landlock.c
@@ -169,8 +169,24 @@ static inline int landlock_restrict_self(const int ruleset_fd, const __u32 flags
 #define LANDLOCK_ACCESS_FS_REFER (1ULL << 13)
 #endif
 
+#ifndef LANDLOCK_ACCESS_FS_TRUNCATE
+#define LANDLOCK_ACCESS_FS_TRUNCATE (1ULL << 14)
+#endif
+
+#ifndef LANDLOCK_ACCESS_FS_IOCTL_DEV
+#define LANDLOCK_ACCESS_FS_IOCTL_DEV (1ULL << 15)
+#endif
+
 #ifndef LANDLOCK_ACCESS_FS_RESOLVE_UNIX
 #define LANDLOCK_ACCESS_FS_RESOLVE_UNIX (1ULL << 18)
+#endif
+
+#ifndef LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET
+#define LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET (1ULL << 0)
+#endif
+
+#ifndef LANDLOCK_SCOPE_SIGNAL
+#define LANDLOCK_SCOPE_SIGNAL (1ULL << 1)
 #endif
 
 #define _LANDLOCK_ACCESS_FS_WRITE                                                                  \
@@ -218,7 +234,9 @@ static inline struct landlock_ruleset *LandlockCreateRuleset(void)
 
     ruleset->attr.handled_access_fs =
             _LANDLOCK_ACCESS_FS_READ | _LANDLOCK_ACCESS_FS_WRITE | LANDLOCK_ACCESS_FS_EXECUTE;
+#ifdef HAVE_LANDLOCK_RULESET_ATTR_HANDLED_ACCESS_NET
     ruleset->attr.handled_access_net = _LANDLOCK_ACCESS_NET;
+#endif
 
     int abi = landlock_create_ruleset(NULL, 0, LANDLOCK_CREATE_RULESET_VERSION);
     if (abi < 0) {
@@ -237,13 +255,17 @@ static inline struct landlock_ruleset *LandlockCreateRuleset(void)
             }
             __attribute__((fallthrough));
         case 3:
+#ifdef HAVE_LANDLOCK_RULESET_ATTR_HANDLED_ACCESS_NET
             ruleset->attr.handled_access_net &= ~_LANDLOCK_ACCESS_NET;
+#endif
             __attribute__((fallthrough));
         case 4:
             ruleset->attr.handled_access_fs &= ~LANDLOCK_ACCESS_FS_IOCTL_DEV;
             __attribute__((fallthrough));
         case 5:
+#ifdef HAVE_LANDLOCK_RULESET_ATTR_SCOPED
             ruleset->attr.scoped &= ~(LANDLOCK_SCOPE_ABSTRACT_UNIX_SOCKET | LANDLOCK_SCOPE_SIGNAL);
+#endif
             __attribute__((fallthrough));
         case 6 ... 8:
             /* Removes LANDLOCK_ACCESS_FS_RESOLVE_UNIX for ABI < 9 */
@@ -408,6 +430,7 @@ void SCLandlockGrantFile(void *vruleset, const char *path, uint32_t access)
     SCLogConfig("Added file permission (0x%x) on '%s'", access, path);
 }
 
+#ifdef HAVE_LANDLOCK_RULESET_ATTR_HANDLED_ACCESS_NET
 static void LandlockGrantNetPort(
         struct landlock_ruleset *ruleset, uint16_t port, uint64_t access, const char *access_name)
 {
@@ -427,10 +450,11 @@ static void LandlockGrantNetPort(
     }
     SCLogConfig("Added net %s permission on port %u", access_name, port);
 }
+#endif
 
 void SCLandlockGrantNetBindTCP(void *vruleset, uint16_t port)
 {
-#ifdef LANDLOCK_ACCESS_NET_BIND_TCP
+#ifdef HAVE_LANDLOCK_RULESET_ATTR_HANDLED_ACCESS_NET
     LandlockGrantNetPort(
             (struct landlock_ruleset *)vruleset, port, LANDLOCK_ACCESS_NET_BIND_TCP, "bind-tcp");
 #else
@@ -441,7 +465,7 @@ void SCLandlockGrantNetBindTCP(void *vruleset, uint16_t port)
 
 void SCLandlockGrantNetConnectTCP(void *vruleset, uint16_t port)
 {
-#ifdef LANDLOCK_ACCESS_NET_CONNECT_TCP
+#ifdef HAVE_LANDLOCK_RULESET_ATTR_HANDLED_ACCESS_NET
     LandlockGrantNetPort((struct landlock_ruleset *)vruleset, port, LANDLOCK_ACCESS_NET_CONNECT_TCP,
             "connect-tcp");
 #else
