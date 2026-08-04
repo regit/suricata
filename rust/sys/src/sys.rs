d599b6131e9ba6f1802277a00ce5a3305383f5e5
Eric Leblond <el@stamus-networks.com>
2026-08-04 14:20:09 +0200

    rust: update plugin system as needed
    As Landlock is introducing new features, we need to update ABI
    and sys.rs.

--- rust/sys/src/sys.rs
+++ rust/sys/src/sys.rs
@@ -84,7 +84,7 @@ extern "C" {
         alproto: AppProto, proto_name: *const ::std::os::raw::c_char,
     );
 }
-pub const SC_API_VERSION: u64 = 2304;
+pub const SC_API_VERSION: u64 = 2305;
 #[doc = " Structure to define a Suricata plugin."]
 #[repr(C)]
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -96,6 +96,9 @@ pub struct SCPlugin_ {
     pub license: *const ::std::os::raw::c_char,
     pub author: *const ::std::os::raw::c_char,
     pub Init: ::std::option::Option<unsafe extern "C" fn()>,
+    #[doc = " Optional callback invoked before landlock sandboxing is enforced.\n  The plugin may grant additional filesystem/network access using the\n  SCLandlockGrant* helpers in util-landlock.h. May be NULL."]
+    pub LandlockEnable:
+        ::std::option::Option<unsafe extern "C" fn(ruleset: *mut ::std::os::raw::c_void)>,
 }
 impl Default for SCPlugin_ {
     fn default() -> Self {
@@ -337,6 +340,11 @@ extern "C" {
     pub fn SCConfNodeLookupChildValue(
         node: *const SCConfNode, key: *const ::std::os::raw::c_char,
     ) -> *const ::std::os::raw::c_char;
+}
+extern "C" {
+    pub fn SCConfNodeLookupInSequence(
+        seq: *const SCConfNode, key: *const ::std::os::raw::c_char, prev: *const SCConfNode,
+    ) -> *mut SCConfNode;
 }
 extern "C" {
     pub fn SCConfNodeRemove(arg1: *mut SCConfNode);
